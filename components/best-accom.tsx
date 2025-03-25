6a4f0c6ffb454121b146c3b770d60604983a753b
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2025-03-26 00:34:50 +0800

    Pull real data from best accommodation

--- components/best-accom.tsx
+++ components/best-accom.tsx
@@ -1,39 +1,11 @@
-import Image from "next/image"
 import React from "react"
-import { Button } from "./ui/button"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
+import { getAccommodations } from "@/app/actions" // Import the function to fetch accommodations
+import CarouselAccom from "./carousel-accom"
 
-const accommodations = [
-  {
-    id: 1,
-    image: "/best-accom1.png",
-    title: "NIPA HUT COTTAGE",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni aspernatur vel fugiat cupiditate doloribus consequatur non ea eveniet ab incidunt nihil itaque ut explicabo repudiandae, debitis blanditiis libero. Voluptas voluptatem debitis deserunt corporis consectetur enim vero eveniet illo at? Sapiente atque incidunt placeat possimus consectetur illo asperiores quis officia totam.",
-  },
-  {
-    id: 2,
-    image: "/best-accom2.png",
-    title: "NIPA HUT COTTAGE",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni aspernatur vel fugiat cupiditate doloribus consequatur non ea eveniet ab incidunt nihil itaque ut explicabo repudiandae, debitis blanditiis libero. Voluptas voluptatem debitis deserunt corporis consectetur enim vero eveniet illo at? Sapiente atque incidunt placeat possimus consectetur illo asperiores quis officia totam.",
-  },
-  {
-    id: 3,
-    image: "/best-accom3.png",
-    title: "NIPA HUT COTTAGE",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni aspernatur vel fugiat cupiditate doloribus consequatur non ea eveniet ab incidunt nihil itaque ut explicabo repudiandae, debitis blanditiis libero. Voluptas voluptatem debitis deserunt corporis consectetur enim vero eveniet illo at? Sapiente atque incidunt placeat possimus consectetur illo asperiores quis officia totam.",
-  },
-]
+export default async function BestAccom() {
+  // Fetch accommodations from the database
+  const accommodations = await getAccommodations()
 
-export default function BestAccom() {
   return (
     <div className="bg-amber-900 py-8 md:py-16">
       <div className="mx-auto w-[80%] text-white">
@@ -41,50 +13,7 @@ export default function BestAccom() {
           See our best Accommodations
         </h1>
 
-        <ul className="gap-6">
-          <Carousel
-            opts={{
-              align: "start",
-            }}
-            className="w-full"
-          >
-            <CarouselContent>
-              {accommodations.map((accom) => (
-                <CarouselItem
-                  key={accom.id}
-                  className="md:basis-1/2 lg:basis-1/3"
-                >
-                  <li key={accom.id}>
-                    <div className="w-full aspect-square mb-3">
-                      <Image
-                        src={accom.image}
-                        alt={accom.title}
-                        width={400}
-                        height={400}
-                        className="size-full object-cover"
-                      />
-                    </div>
-                    <h3 className="font-bold tracking-tight mb-3 text-center text-lg">
-                      {accom.title}
-                    </h3>
-                    <p className="mb-4 opacity-80 text-sm">
-                      {accom.description}
-                    </p>
-                    <Button
-                      type="button"
-                      variant={"secondary"}
-                      className="mx-auto w-full"
-                    >
-                      More info
-                    </Button>
-                  </li>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious className="left-[-10%] lg:left-[-5%]" />
-            <CarouselNext className="right-[-10%] lg:right-[-5%]" />
-          </Carousel>
-        </ul>
+        <CarouselAccom accommodations={accommodations} />
       </div>
     </div>
   )
