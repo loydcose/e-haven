a49616e12de7f16c7425fc05842970604e0994ae
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2025-04-02 03:56:16 +0800

    Add loaders

--- app/admin/tables/accommodations/submit-button.tsx
+++ app/admin/tables/accommodations/submit-button.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button"
 import { updateAccommodation } from "@/app/actions"
 import { Fields } from "./accommodations-action"
 import { useToast } from "@/hooks/use-toast"
+import { useState } from "react"
+import Spinner from "@/components/icons/spinner"
 
 export default function SubmitButton({
   hasChange,
@@ -15,28 +17,47 @@ export default function SubmitButton({
   fields: Fields
 }) {
   const { toast } = useToast()
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleClick = async () => {
-    const result = await updateAccommodation(accommodationId, fields)
-    console.log(fields)
-    if (result.success) {
-      toast({
-        title: "Success",
-        description: result.message,
-        variant: "success",
-      })
-    } else {
+    setIsLoading(true) // Show the spinner
+    try {
+      const result = await updateAccommodation(accommodationId, fields)
+      if (result.success) {
+        toast({
+          title: "Success",
+          description: result.message,
+          variant: "success",
+        })
+        setTimeout(() => {
+          window.location.href = window.location.href
+        }, 1000)
+      } else {
+        toast({
+          title: "Error",
+          description: result.message,
+          variant: "destructive",
+        })
+      }
+    } catch (error) {
+      console.error("Error updating accommodation:", error)
       toast({
         title: "Error",
-        description: result.message,
+        description: "An unexpected error occurred.",
         variant: "destructive",
       })
+    } finally {
+      setIsLoading(false) // Hide the spinner
     }
   }
 
   return (
-    <Button type="button" onClick={handleClick} disabled={!hasChange}>
-      {"Save changes"}
+    <Button
+      type="button"
+      onClick={handleClick}
+      disabled={!hasChange || isLoading}
+    >
+      {isLoading ? <Spinner /> : "Save changes"}
     </Button>
   )
 }
